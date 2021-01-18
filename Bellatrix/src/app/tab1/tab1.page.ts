6a4afee016c91c81a62fb2ab983cd96347bf1c25
pwnGIGO <Rodrigo>
2021-01-18 02:52:21 -0600

    Se agrego parte de la ordenacion y el filtrado

--- Bellatrix/src/app/tab1/tab1.page.ts
+++ Bellatrix/src/app/tab1/tab1.page.ts
@@ -7,6 +7,84 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  lista = [];
+
+	constructor() {
+    	this.lista  = [
+			{
+				"nombre": "Ven aqui",
+				"artista": "Los Bunkers"
+			},
+			{
+				"nombre": "Cita en el quirofano",
+				"artista": "Pxndx"	
+			},
+			{
+				"nombre": "With or without you",
+				"artista": "U2"	
+			}
+		];
+
+		console.log(this.lista);
+  	}
+
+  	// Barra de busqueda
+  	buscar($event){
+  		// Si no hay texto, muestra todo el playlist
+  		if($event.target.value == ""){
+  			this.lista  = [
+				{
+					"nombre": "Ven aqui",
+					"artista": "Los Bunkers"
+				},
+				{
+					"nombre": "Cita en el quirofano",
+					"artista": "Pxndx"	
+				},
+				{
+					"nombre": "With or without you",
+					"artista": "U2"	
+				}
+			];
+  		}else{ // Se muestran las coicidencias del texto
+  			// LAS COINCIDENCIAS LAS OBENETEMOS DE LA CONSULTA DEL MONGO
+	  		this.lista = [
+		  		{
+		  			"nombre": "buscando",
+		  			"artista": "buscando"
+		  		}
+	  		]
+  		}
+  	}
+
+  	// Recibe el evento de seleccionar el ordenamiento
+  	ordenarPor($event){
+  		// LA ORDENACION SE REALIZA CON UNA CONSULTA DE MONGO
+  		switch ($event.target.value) {
+  			case "artista":
+  				this.porArtista();
+  				break;
+  			case "genero":
+  				this.porGenero();
+  				break;
+  			case "nombre":
+  				this.porNombre();
+  				break;
+  		}
+  	}
+  	porArtista(){
+  		console.log("Ordenando por artista");
+  	}
+
+  	porGenero(){
+  		console.log("Ordenando por genero");
+  	}
+
+  	porNombre(){
+  		console.log("Ordenando por nombre");
+  		//Prueba de actualizacion de la lista
+  		let nuevaLista = [this.lista[1], this.lista[0], this.lista[2]];
+  		this.lista = nuevaLista;
+  	}
 
 }
